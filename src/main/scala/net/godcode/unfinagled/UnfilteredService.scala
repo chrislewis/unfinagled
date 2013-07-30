446672663fd084a9eb4cd463cdb435f6b5c1b77b
Dag Liodden <dag@tapad.com>
2013-07-30 16:04:14 -0400

    Allow intents to be applied in FuturePools

--- src/main/scala/net/godcode/unfinagled/UnfilteredService.scala
+++ src/main/scala/net/godcode/unfinagled/UnfilteredService.scala
@@ -1,28 +1,34 @@
 package net.godcode.unfinagled
 
 import com.twitter.finagle.Service
-import com.twitter.util.Future
+import com.twitter.util.{FuturePool, Future}
 import org.jboss.netty.handler.codec.http._
 import org.jboss.netty.handler.codec.http.HttpVersion._
 import org.jboss.netty.handler.codec.http.HttpResponseStatus._
 import unfiltered.response.{Connection, ContentLength, NotFound, Responder, HttpResponse => UHttpResponse}
-import unfiltered.netty.ResponseBinding
-import unfiltered.netty.cycle.Plan.Intent
+import unfiltered.netty._
 
 /**
- * Functions for constructing finagle Services from unfiltered types.
+ * Functions for constructing finagle Services from Unfiltered types.
  */
 object UnfilteredService {
 
   val nettyResponse: HttpResponseStatus => HttpResponse =
     new DefaultHttpResponse(HTTP_1_1, _)
 
-  def apply(intent: Intent) =
+  /**
+   * Creates a Finagle wrapper service around a synchronous Unfiltered intent.
+   *
+   * @param intent the intent to wrap
+   * @param futurePool the pool on which the intent will be applied. The default assumes the intent is non-blocking.
+   * @return the Finagle service wrapper
+   */
+  def apply(intent: cycle.Plan.Intent, futurePool: FuturePool = FuturePool.immediatePool) : Service[HttpRequest, HttpResponse] =
     new Service[HttpRequest, HttpResponse] {
       def apply(request: HttpRequest): Future[HttpResponse] =
         request match {
           case uf: RequestAdapter =>
-            Future {
+            futurePool {
               val rf = intent.lift(uf.binding).getOrElse(NotFound)
               val nres = new ResponseBinding(nettyResponse(OK))
               val keepAlive = HttpHeaders.isKeepAlive(request)
@@ -44,4 +50,5 @@ object UnfilteredService {
             Future(nettyResponse(INTERNAL_SERVER_ERROR))
         }
     }
+
 }
